26711c76882e8e10ec6177b6d1895b69905839b1
Nicky-muindi <nicholasmuindi25@gmail.com>
2021-09-08 06:51:19 +0000

    Prints the alphabet only with a format

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#2-priny_alphabet.py
 """Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
-      print("{}".format(chr(letter)), end="")
+    print("{}".format(chr(letter)), end="")
