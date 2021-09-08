99e273f8f8fbbefd3b5646e33f4d7b1157948c15
Nicky-muindi <nicholasmuindi25@gmail.com>
2021-09-08 08:28:57 +0000

    Prints only the numbers that the first digit will be greater than the

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 99:
-        print('{:02d}, '.format(i), end='')
-    else:
-        print(i)
+for i in range(10):
+    for j in range(10):
+        if (i != j and i < j) and i < 9:
+            if (i == 8 and j == 9):
+                print('{0}{1}'.format(i, j))
+            else:
+                print('{0}{1}, '.format(i, j), end='')
