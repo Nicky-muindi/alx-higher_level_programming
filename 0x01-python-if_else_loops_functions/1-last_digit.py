de86be33c077bf25b0cd717dfea543063e82096b
Nicky-muindi <nicholasmuindi25@gmail.com>
2021-09-08 06:18:29 +0000

    Fixes the last digit printed of a negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,4 @@ elif last_digit < 6 and last_digit != 0:
    str = 'Last digit of {0} is {1} and is less than 6 and not 0'
 elif last_digit == 0:
    str = 'Last digit of {0} is {1} and is 0'
-
 print(str.format(number, last_digit))
