11ac93d28d2c4ed101dc6f091896019c68205bdd
Nicky-muindi <nicholasmuindi25@gmail.com>
2021-09-10 18:55:23 +0000

    Fixes the condition to get an element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
+# 1-element_at.py
 
-    return my_list[idx]
+
+def element_at(my_list, idx):
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
