e5e8e0bd27b011419073e579e1a14efd9bbe9209
Nicky-muindi <nicholasmuindi25@gmail.com>
2021-10-12 06:47:59 +0000

    Python-inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Nicholas M Mwanza <nicholasmuindi25@gmail.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
