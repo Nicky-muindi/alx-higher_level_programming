1962ee10f29df4fc1216064285778629a07b0e1e
Nicky-muindi <nicholasmuindi25@gmail.com>
2021-11-17 03:54:26 -0800

    restored

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,7 +1,2 @@
--- Deletes the database `hbtn_0c_0` in MySQL Server.
---      If the database `hbtn_0c_0` doesn't exists, the script shouldn't fail
-create random password
-PASSWDDB="$(openssl rand -base64 12)"
-MAINDB=${user_0d_1//[user_0d_1_pwd}
-mysql -e "CREATE DATABASE $hbtn_0c_0"
-mysql -e "GRANT ALL PRIVILEGES ON $hbtn_0c_0.* TO $hbtn_0c_0@localhost IDENTIFIED BY '$PASSWDDB'"
+-- Creates the database hbtn_0c_0.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,2 @@
--- Deletes the database `hbtn_0c_0` in MySQL Server.
---      If the database `hbtn_0c_0` doesn't exists, the script shouldn't fail
-DROP DATABASE IF EXISTS hbtn_0c_0;
+-- Deletes the database hbtn_0c_0.
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
